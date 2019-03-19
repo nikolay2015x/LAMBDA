c724cfed568cce19152f5cc9e88f3ebddaa6278a
nikolay2015x <nikolaiunited2015@mail.ru>
2019-03-19 16:11:43 +0300

    add: footer: menu and mobile-menu

---
+++ js/JavaScr.js
@@ -0,0 +1,18 @@
+// function openbox (box) {
+//     display = document.getElementById('box').style.display;
+//     if (display == 'block'){
+//         document.getElementById('box').style.display = 'none';
+//     } else {
+//         document.getElementById('box').style.display = 'block';
+//     }
+// }
+
+
+function openbox (box){
+    display = document.getElementById('box').style.display;
+    if (display == 'block'){
+        document.getElementById('box').style.display = 'none';
+    } else {
+        document.getElementById('box').style.display = 'block';
+    }
+}
